1ae3c8f199cce5069dbec80d904b2e5079d216aa
Génesis M <genesis.medina@unitec.edu>
2024-11-09 11:49:21 -0600

    Barra de navegación y unión de las pantallas

---
+++ lib/sections/drinkdetails.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DrinkDetails extends StatelessWidget {
+  const DrinkDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Drink Details Page'),
+    );
+  }
+}
---
+++ lib/sections/drinklist.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DrinkList extends StatelessWidget {
+  const DrinkList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Drink List Page'),
+    );
+  }
+}
